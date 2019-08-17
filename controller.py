39d65e2620f25ee490f83f641c5ec7d5adda08cf
Ryan Keys <r.keys1998@gmail.com>
2019-08-16 19:52:46 -0700

    cleaning up half written code

--- controller.py
+++ controller.py
@@ -100,7 +100,6 @@ class FrontEnd(object):
                               (x+w, y+h), (255, 0, 0), 2)
                 roi_gray = gray[y:y+h, x:x+w]
                 roi_color = frame[y:y + h, x:x + w]
-                out.write(frame.faces)
                 eyes = eye_cascade.detectMultiScale(roi_gray)
                 for (ex, ey, ew, eh) in eyes:
                     cv2.rectangle(roi_color, (ex, ey),
